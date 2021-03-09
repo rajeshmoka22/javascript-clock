f97171e6cacbf1942d67e113112c22f6f2adae34
Rajesh Moka <rajeshmoka@icloud.com>
2021-03-09 22:52:18 +0530

    added styles and digital clock

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let secondsHand = document.querySelector(".seconds-hand");
 let minutesHand = document.querySelector(".minutes-hand");
 let hoursHand = document.querySelector(".hours-hand");
+const hoursEle = document.querySelector('.hours');
+const minutesEle = document.querySelector('.minutes');
+const secondsEle = document.querySelector('.seconds');
 setInterval(setDate,1000);
 
 function setDate(){
@@ -12,4 +15,7 @@ function setDate(){
   secondsHand.style.transform = "rotate("+((seconds/60*360)+90)+"deg)";
   minutesHand.style.transform = "rotate("+((minutes/60*360)+90)+"deg)";
   hoursHand.style.transform = "rotate("+((hours/12*360)+90)+"deg)";
+  hoursEle.innerText = hours;
+  minutesEle.innerText = minutes;
+  secondsEle.innerText = seconds > 10 ? seconds : "0"+seconds;
 }
